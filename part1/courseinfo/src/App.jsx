719ea1e55bef263dd01850208ef80c714d9fbba4
alexandru calin <alexandrucalin@proton.me>
2023-10-25 16:23:09 +0300

    finished exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,17 +4,11 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
-    <>
-      <p>
-        {props.title1} {props.exercises1}
-      </p>
-      <p>
-        {props.title2} {props.exercises2}
-      </p>
-      <p>
-        {props.title3} {props.exercises3}
-      </p>
-    </>
+    <div>
+      <Part title={props.title1} exercises={props.exercises1} />
+      <Part title={props.title2} exercises={props.exercises2} />
+      <Part title={props.title3} exercises={props.exercises3} />
+    </div>
   );
 };
 
@@ -23,6 +17,14 @@ const Total = (props) => {
     <p>
       Number of exercises{" "}
       {props.exercises1 + props.exercises2 + props.exercises3}
+    </p>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.title} {props.exercises}
     </p>
   );
 };
