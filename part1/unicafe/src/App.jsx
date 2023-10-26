3c12b13416b398edd107a672a5689ee42f8304f4
alexandru calin <alexandrucalin@proton.me>
2023-10-26 20:02:16 +0300

    starting point of exercise 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+
 const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
   return (
     <div>
-      <p>hello, world</p>
+      <h1>give feedback</h1>
+      <h1>statistics</h1>
     </div>
   );
 };
